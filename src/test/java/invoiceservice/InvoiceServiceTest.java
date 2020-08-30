203d21b001a1933c0a58e29c9441a86010abdecb
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-31 03:14:28 +0530

    Refactored the code to remove Exception and Added the test case to check minimum fare is Rs 5

--- src/test/java/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/invoiceservice/InvoiceServiceTest.java
@@ -13,12 +13,18 @@ public class InvoiceServiceTest {
     @Mock
     private InvoiceGenerator invoiceGenerator;
     @Before
-    public void setInvoiceGenerator() throws Exception {
+    public void setInvoiceGenerator() {
         invoiceGenerator = new InvoiceGenerator();
     }
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         double totalFare = invoiceGenerator.calculateFare(2, 5);
         Assert.assertEquals(25, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceOrTime_WhenLess_ShouldReturnMinimumFare() {
+        double totalFare = invoiceGenerator.calculateFare(0.1, 1);
+        Assert.assertEquals(5, totalFare, 0.0);
     }
 }
