3869e94afd09c626ac5d596a9cad45412c32fbcd
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-31 03:17:00 +0530

    Added code to return minimum fare if calculated fare is less than the minimum fare

--- src/main/java/invoiceservice/InvoiceGenerator.java
+++ src/main/java/invoiceservice/InvoiceGenerator.java
@@ -3,8 +3,12 @@ package invoiceservice;
 public class InvoiceGenerator {
     public static final int COST_PER_TIME = 1;
     public static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
